0bb61ebb6222517bae8d2954d85026082d799f7c
Raghu Betina <rbetina@gmail.com>
2012-08-31 15:18:03 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chess::Application.config.secret_token = 'cb7bb9ea6892cdf2bbc2d2b47460be331017bd02b68bc33232edfcaf5a661d09ff6ed67fa6269214530c7d7e9d2c248bdac7886ac425b0bb008dd6b3255793d7'
