b0288763c260f61cc41ac2273ea4cdda523fb6f0
Raghu Betina <rbetina@gmail.com>
2012-09-01 19:33:24 -0500

    Removed extraneous scaffolding.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,32 +14,14 @@ class GamesController < ApplicationController
   # GET /games/1.json
   def show
     @game = Game.find(params[:id])
-    @moves = @game.moves
+    @moves = @game.moves.order("created_at ASC")
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @game }
+      format.json { render json: @moves }
     end
   end
-
-  # GET /games/new
-  # GET /games/new.json
-  def new
-    @game = Game.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @game }
-    end
-  end
-
-  # GET /games/1/edit
-  def edit
-    @game = Game.find(params[:id])
-  end
-
-  # POST /games
-  # POST /games.json
+  
   def create
     @game = Game.new(params[:game])
 
@@ -48,23 +30,7 @@ class GamesController < ApplicationController
         format.html { redirect_to @game, notice: 'Game was successfully created.' }
         format.json { render json: @game, status: :created, location: @game }
       else
-        format.html { render action: "new" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /games/1
-  # PUT /games/1.json
-  def update
-    @game = Game.find(params[:id])
-
-    respond_to do |format|
-      if @game.update_attributes(params[:game])
-        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
+        format.html { redirect_to games_url}
         format.json { render json: @game.errors, status: :unprocessable_entity }
       end
     end
